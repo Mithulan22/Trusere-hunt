5d01059c4c6c419c9c8be53c6370844316e9200b
Mithulan22 <76974205+Mithulan22@users.noreply.github.com>
2021-07-05 16:43:22 -0400

    Add files via upload

---
+++ js/System.js
@@ -0,0 +1,14 @@
+class System{
+
+    constructor(){}
+
+    // Add code to authenticate the given code and the access codes.
+authenticate(actualCode,enteredcode ){
+if(actualCode === enteredcode)
+     return true 
+     else 
+      return false
+}
+    
+
+}
